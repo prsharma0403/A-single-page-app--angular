7b9faac25a2a2f27347520c82893cfd2cdc992c8
prsharma0403 <prsharma0403@gmail.com>
2016-12-25 19:09:42 +0530

    commiting for single page using angular

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyASmqOd11NS_AgZwz7ylwG-76YblUbExN8",
+    authDomain: "fireapp-65c92.firebaseapp.com",
+    databaseURL: "https://fireapp-65c92.firebaseio.com",
+    storageBucket: "fireapp-65c92.appspot.com",
+    messagingSenderId: "27535690383"
+  };
+  firebase.initializeApp(config);
